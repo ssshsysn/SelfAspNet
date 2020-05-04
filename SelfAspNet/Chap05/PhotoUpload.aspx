68c149f0ff1fef91929c68e6010413d6e2051ad4
ssshsysn <ssshsysn@gmail.com>
2020-05-05 08:05:08 +0900

    5.1.2 データベースにバイナリデータを登録する

---
+++ SelfAspNet/Chap05/PhotoUpload.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhotoUpload.aspx.cs" Inherits="SelfAspNet.Chap05.PhotoUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="upfile" runat="server" />
+            <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="アップロード" />
+            <br />
+            <asp:SqlDataSource ID="sds" runat="server" ConnectionString="<%$ ConnectionStrings:SelfApp %>" InsertCommand="INSERT INTO Photo(type, data) VALUES (@type, @data)" ProviderName="<%$ ConnectionStrings:SelfApp.ProviderName %>">
+                <InsertParameters>
+                    <asp:ControlParameter ControlID="upfile" Name="data" PropertyName="FileBytes" />
+                </InsertParameters>
+            </asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SelfAspNet/Chap05/PhotoUpload.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNet.Chap05
+{
+
+
+    public partial class PhotoUpload
+    {
+
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// upfile コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload upfile;
+
+        /// <summary>
+        /// btnUpload コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUpload;
+
+        /// <summary>
+        /// sds コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource sds;
+    }
+}
