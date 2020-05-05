583b7bf005b362a340b7d1f5af78e2b824c7a428
ssshsysn <ssshsysn@gmail.com>
2020-05-05 09:19:51 +0900

    5.2.4 接続型アクセスによる参照処理

---
+++ SelfAspNet/App_Data/SQLQuery6.sql
@@ -0,0 +1,17 @@
+﻿USE [C:\USERS\SSSHS\SOURCE\REPOS\SELFASPNET\SELFASPNET\APP_DATA\SELFASP.MDF]
+GO
+
+DECLARE	@return_value Int,
+		@recnum int
+
+SELECT	@recnum = NULL
+
+EXEC	@return_value = [dbo].[AlbumFilter]
+		@category = N'動物',
+		@recnum = @recnum OUTPUT
+
+SELECT	@recnum as N'@recnum'
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ SelfAspNet/Chap05/ExecuteReader.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SelfAspNet.Chap05
+{
+    public partial class ExecuteReader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // データベース接続情報を取得
+            var setting = ConfigurationManager.ConnectionStrings["SelfApp"];
+
+            // 接続準備
+            using(var db= new SqlConnection(setting.ConnectionString))
+            {
+                // SQL ORDER
+                var comm = new SqlCommand("SELECT * FROM Book WHERE publish = @publish", db);
+                comm.Parameters.AddWithValue("@publish", "翔泳社");
+                // 接続の確立
+                db.Open();
+                // SQL命令を実行し、その結果を表示
+                var reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    Response.Write($"{reader["title"]}<br />");
+                }
+                db.Close();
+            }
+        }
+    }
+}
