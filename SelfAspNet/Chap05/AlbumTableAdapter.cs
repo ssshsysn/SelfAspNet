8afb5955615aab5891553bf485e1f5026359884b
ssshsysn <ssshsysn@gmail.com>
2020-05-06 12:03:04 +0900

    5.3.2 データアクセスコンポーネント開発

---
+++ SelfAspNet/Chap05/AlbumTableAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SelfAspNet.Chap05.AlbumDataSetTableAdapters
+{
+    public partial class AlbumTableAdapter
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, true)]
+        public DataSet GetAlbumData(String category)
+        {
+            ConnectionStringSettings setting =
+                ConfigurationManager.ConnectionStrings["SelfApp"];
+            var db = new SqlConnection(setting.ConnectionString);
+            var comm = new SqlCommand();
+            comm.Connection = db;
+            if (category == "無選択")
+            {
+                comm.CommandText = "SELECT aid, comment, updated, favorite, category FROM Album";
+            }
+            else
+            {
+                comm.CommandText = "SELECT aid, comment, updated, favorite, category FROM Album WHERE category = @category";
+                comm.Parameters.AddWithValue("@category", category);
+            }
+            var ds = new DataSet();
+            var adapter = new SqlDataAdapter(comm);
+            adapter.Fill(ds, "Album");
+            return ds;
+        }
+
+        //[DataObjectMethod(DataObjectMethodType.Select, false)]
+        //public SelfAspNet.Chap05.AlbumDataset.AlbumDataTable GetAlbumPaging(int startRowIndex, int maximumRows)
+        //{
+            //SqlCommand comm = this.Connection.CreateCommand();
+            //comm.CommandText = $"SELECT aid, comment, updated, favorite, category FROM Album ORDER BY updated DESC OFFSET {startRowIndex} ROWS FETCH NEXT {maximumRows} ROWS ONLY";
+            //this.Adapter.SelectCommand = comm;
+            //AlbumDataset ds = new AlbumDataset();
+            //this.Adapter.Fill(ds, "Album");
+            //return ds.Album;
+        //}
+    }
+}
---
+++ SelfAspNet/Chap05/ObjectParam2.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ObjectParam2.aspx.cs" Inherits="SelfAspNet.Chap05.ObjectParam2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButtonList ID="list" runat="server" AppendDataBoundItems="True" AutoPostBack="True" DataSourceID="sds" DataTextField="category" DataValueField="category" RepeatDirection="Horizontal">
+                <asp:ListItem Selected="True">無選択</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="obj">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
